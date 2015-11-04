3aa59d167bf8102704a0088a472a83c0f9e788ed
Chris Henrick <chrishenrick@gmail.com>
2015-11-04 18:38:22 -0500

    add new sql query using st_dwithin()

--- map/src/select_from_buffer.sql
+++ map/src/select_from_buffer.sql
@@ -12,3 +12,23 @@ WHERE ST_Within(
     1000
   )
 )
+
+-- return true if features intersect
+SELECT 
+  a.after_d_01, 
+  a.before__01, 
+  a.cartodb_id, 
+  a.the_geom_webmercator, 
+  within
+FROM (
+  SELECT *,
+  ST_DWithin(
+    the_geom_webmercator,
+    ST_Transform(
+      ST_GeomFromText('Point(-73.925028 40.694631)', 4326),
+    3857),
+    741
+  ) as within
+  FROM nyc_flips_pluto_150712
+) as a
+WHERE a.within = true
